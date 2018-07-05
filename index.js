26084f2c81fecab4e33580f69d738db1c7dc5ba8
Brock Tubre <brock.tubre@csra.com>
2018-07-05 11:58:49 -0500

    batch creates

--- index.js
+++ index.js
@@ -8,33 +8,39 @@ const QR_URL = 'http://api.qrserver.com/v1/create-qr-code/';
 const QR_BUCKET = 'qr-codes-bucket';
 
 exports.handler = function(event, context) {
-    let qrUrl = QR_URL;
-    let email = event.email;
-
     const d = new Date();
     d.setHours(d.getHours() - 5);
     const todaysDateId = moment(d.toISOString()).format("MM-DD-YYYY");
 
-    let key = todaysDateId + '_' + email + '.png';
-    qrUrl += '?data=' + key + '!&size=100x100';
-    console.log(qrUrl);
-    const qrCodeRequest = request.get(qrUrl, {followRedirect: false});
 
-    return qrCodeRequest.then((img) => {
+    event.students.forEach(item => {
+        let key = todaysDateId + '_' + item.student_id + '.png';
+        let qrUrl = QR_URL;
+        qrUrl += '?data=' + key + '!&size=100x100';
+        console.log(qrUrl);
+        const qrCodeRequest = request.get(qrUrl, {followRedirect: false});
 
-        const buffer = Buffer.from(img, 'base64');
-        var data = {
-            Bucket: QR_BUCKET, 
-            Key: key, 
-            Body: buffer,
-            ContentType: 'image/png',
-            ACL: 'public-read'
-          };
-        const putObject = s3.putObject(data).promise();
+        return qrCodeRequest.then((img) => {
+                const buffer = Buffer.from(img, 'base64');
+                var data = {
+                    Bucket: QR_BUCKET, 
+                    Key: key, 
+                    Body: buffer,
+                    ContentType: 'image/png',
+                    ACL: 'public-read',
+                    Metadata: {
+                        'students_first_name' : item.first_name,
+                        'students_last_name': item.last_name,
+                        'students_id': item.student_id,
+                        'students_email': item.email
+                    }
+                };
+                const putObject = s3.putObject(data).promise();
 
-        return putObject.then((data, err) => {
-            if(err) console.err(err);
-            return key;
+                return putObject.then((data, err) => {
+                    if(err) console.err(err);
+                    return key;
+                });
+            });
         });
-    });
 };
