9c01e919e8f210c1c224a7d954c59bf1134c454e
Brock Tubre <brock.tubre@csra.com>
2018-07-05 15:06:58 -0500

    Progress

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var AWS = require('aws-sdk');
 var request = require('request-promise').defaults({ encoding: null });
 var moment = require('moment');
 var s3 = new AWS.S3();
+const SES = new AWS.SES({apiVersion: '2010-12-01'});
 
 const QR_URL = 'http://api.qrserver.com/v1/create-qr-code/';
 const QR_BUCKET = 'qr-codes-bucket';
@@ -39,7 +40,9 @@ exports.handler = function(event, context) {
 
                 return putObject.then((data, err) => {
                     if(err) console.err(err);
-                    return key;
+                    else {
+                        return key;
+                    }
                 });
             });
         });
